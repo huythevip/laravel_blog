129b7c62fd6b8e9183e71be92a892c13288183a2
An Vo <an.vo@hotelquickly.com>
2018-03-17 09:56:34 +0700

    Change database posts table

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'title' => $faker->sentence,
         'content' => $faker->paragraph,
         // 'id' => function () {
         //     return factory(App\Post::class)->create()->id;
